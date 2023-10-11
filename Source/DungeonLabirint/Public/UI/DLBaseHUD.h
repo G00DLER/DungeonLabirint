d34efbae67a0a5b8dc3e091f5563d5f85819c7f6
G00DLER <n1kitaartamonov@ya.ru>
2023-10-11 22:13:18 +0500

    Added HUD class

---
+++ Source/DungeonLabirint/Public/UI/DLBaseHUD.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "DLBaseHUD.generated.h"
+
+
+UCLASS()
+class DUNGEONLABIRINT_API ADLBaseHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
